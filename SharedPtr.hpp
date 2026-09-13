0889387024eede0bb5d6fcdb71859d1138e506ac
Edward <eduardsivtsev@mail.ru>
2026-09-13 18:43:09 +0300

    added pointers for arrays

--- SharedPtr.hpp
+++ SharedPtr.hpp
@@ -1,64 +1,206 @@
+#include <cstddef>
+#include "Exceptions.hpp"
+
 template <typename T>
 class SharedPtr
 {
 private:
     T *ptr;
-    int *ref_count;
+    size_t *ref_count;
+
+    void release() noexcept
+    {
+        if (ref_count != nullptr && --(*ref_count) == 0)
+        {
+            delete ptr;
+            delete ref_count;
+        }
+        ptr = nullptr;
+        ref_count = nullptr;
+    }
 
 public:
-    SharedPtr(T *p = nullptr) : ptr(p), ref_count(new int(1)) {}
+    SharedPtr(T *p = nullptr) : ptr(p), ref_count(nullptr)
+    {
+        if (p != nullptr)
+            ref_count = new size_t(1);
+    }
 
     SharedPtr(const SharedPtr &other)
         : ptr(other.ptr), ref_count(other.ref_count)
     {
-        ++(*ref_count);
+        if (ref_count != nullptr)
+            ++(*ref_count);
+    }
+
+    SharedPtr(SharedPtr &&other) noexcept
+        : ptr(other.ptr), ref_count(other.ref_count)
+    {
+        other.ptr = nullptr;
+        other.ref_count = nullptr;
     }
 
     SharedPtr &operator=(const SharedPtr &other)
     {
         if (this != &other)
         {
-            if (--(*ref_count) == 0)
-            {
-                delete ptr;
-                delete ref_count;
-            }
+            release();
             ptr = other.ptr;
             ref_count = other.ref_count;
-            ++(*ref_count);
+            if (ref_count != nullptr)
+                ++(*ref_count);
+        }
+        return *this;
+    }
+
+    SharedPtr &operator=(SharedPtr &&other) noexcept
+    {
+        if (this != &other)
+        {
+            release();
+            ptr = other.ptr;
+            ref_count = other.ref_count;
+            other.ptr = nullptr;
+            other.ref_count = nullptr;
         }
         return *this;
     }
 
     ~SharedPtr()
     {
-        if (--(*ref_count) == 0)
-        {
-            delete ptr;
-            delete ref_count;
-        }
+        release();
     }
 
     T &operator*() const { return *ptr; }
     T *operator->() const { return ptr; }
-    T *get() const { return ptr; }
+    T *get() const noexcept { return ptr; }
+
+    size_t getCountRef() const noexcept
+    {
+        return ref_count != nullptr ? *ref_count : 0;
+    }
 
     void reset(T *p = nullptr)
     {
         if (ptr != p)
         {
-            if (--(*ref_count) == 0)
-            {
-                delete ptr;
-                delete ref_count;
-            }
+            release();
             ptr = p;
             if (p != nullptr)
-            {
-                ref_count = new int(1);
-            }
-            else
-                ref_count = nullptr;
+                ref_count = new size_t(1);
+        }
+    }
+};
+
+/////////////////////////////////////////
+
+template <typename T>
+class SharedPtrArr
+{
+private:
+    T *ptr;
+    size_t *ref_count;
+    size_t size;
+
+    void release() noexcept
+    {
+        if (ref_count != nullptr && --(*ref_count) == 0)
+        {
+            delete[] ptr;
+            delete ref_count;
+        }
+        ptr = nullptr;
+        ref_count = nullptr;
+        size = 0;
+    }
+
+public:
+    SharedPtrArr(T *p = nullptr, size_t s = 0) : ptr(p), ref_count(nullptr), size(s)
+    {
+        if (p != nullptr)
+            ref_count = new size_t(1);
+    }
+
+    SharedPtrArr(const SharedPtrArr &other)
+        : ptr(other.ptr), ref_count(other.ref_count), size(other.size)
+    {
+        if (ref_count != nullptr)
+            ++(*ref_count);
+    }
+
+    SharedPtrArr(SharedPtrArr &&other) noexcept
+        : ptr(other.ptr), ref_count(other.ref_count), size(other.size)
+    {
+        other.ptr = nullptr;
+        other.ref_count = nullptr;
+        other.size = 0;
+    }
+
+    SharedPtrArr &operator=(const SharedPtrArr &other)
+    {
+        if (this != &other)
+        {
+            release();
+            ptr = other.ptr;
+            ref_count = other.ref_count;
+            size = other.size;
+            if (ref_count != nullptr)
+                ++(*ref_count);
+        }
+        return *this;
+    }
+
+    SharedPtrArr &operator=(SharedPtrArr &&other) noexcept
+    {
+        if (this != &other)
+        {
+            release();
+            ptr = other.ptr;
+            ref_count = other.ref_count;
+            size = other.size;
+            other.ptr = nullptr;
+            other.ref_count = nullptr;
+            other.size = 0;
+        }
+        return *this;
+    }
+
+    ~SharedPtrArr()
+    {
+        release();
+    }
+
+    T &operator[](size_t index)
+    {
+        if (ptr == nullptr || index >= size)
+            throw IndexOutOfRange("SharedPtrArr::operator[]: индекс вне диапазона");
+        return ptr[index];
+    }
+
+    const T &operator[](size_t index) const
+    {
+        if (ptr == nullptr || index >= size)
+            throw IndexOutOfRange("SharedPtrArr::operator[]: индекс вне диапазона");
+        return ptr[index];
+    }
+
+    T *get() const noexcept { return ptr; }
+    size_t getSize() const noexcept { return size; }
+
+    size_t getCountRef() const noexcept
+    {
+        return ref_count != nullptr ? *ref_count : 0;
+    }
+
+    void reset(T *p = nullptr, size_t s = 0)
+    {
+        if (ptr != p || size != s)
+        {
+            release();
+            ptr = p;
+            size = s;
+            if (p != nullptr)
+                ref_count = new size_t(1);
         }
     }
 };
--- UniquePtr.hpp
+++ UniquePtr.hpp
@@ -1,42 +1,118 @@
+#include <cstddef>
+#include "Exceptions.hpp"  
+
 template <typename T>
 class UniquePtr
 {
 private:
     T *ptr;
 
+    void release() noexcept
+    {
+        delete ptr;
+        ptr = nullptr;
+    }
+
 public:
-    UniquePtr(T *p = nullptr) : ptr(p) {}
-    ~UniquePtr() { delete ptr; }
+    UniquePtr(T *p = nullptr) noexcept : ptr(p) {}
+
+    ~UniquePtr() { release(); }
+
     UniquePtr(const UniquePtr &) = delete;
     UniquePtr &operator=(const UniquePtr &) = delete;
+
     UniquePtr(UniquePtr &&other) noexcept : ptr(other.ptr)
     {
         other.ptr = nullptr;
     }
+
     UniquePtr &operator=(UniquePtr &&other) noexcept
     {
         if (this != &other)
         {
-            delete ptr;
+            release();
             ptr = other.ptr;
             other.ptr = nullptr;
         }
         return *this;
     }
+
     T &operator*() const { return *ptr; }
-    T *operator->() const { return ptr; }
-    T *get() const { return ptr; }
-    T *release()
+    T *operator->() const noexcept { return ptr; }
+    T *get() const noexcept { return ptr; }
+
+    T *release() noexcept
     {
         T *tmp = ptr;
         ptr = nullptr;
         return tmp;
     }
-    void reset(T *p = nullptr)
+
+    void reset(T *p = nullptr) noexcept
     {
         if (ptr != p)
         {
-            delete ptr;
+            release();
+            ptr = p;
+        }
+    }
+};
+
+/////////////////////////////////////////
+
+template <typename T>
+class UniquePtrArr
+{
+private:
+    T *ptr;
+
+    void release() noexcept
+    {
+        delete[] ptr;
+        ptr = nullptr;
+    }
+
+public:
+    UniquePtrArr(T *p = nullptr) noexcept : ptr(p) {}
+
+    ~UniquePtrArr() { release(); }
+
+    UniquePtrArr(const UniquePtrArr &) = delete;
+    UniquePtrArr &operator=(const UniquePtrArr &) = delete;
+
+    UniquePtrArr(UniquePtrArr &&other) noexcept : ptr(other.ptr)
+    {
+        other.ptr = nullptr;
+    }
+
+    UniquePtrArr &operator=(UniquePtrArr &&other) noexcept
+    {
+        if (this != &other)
+        {
+            release();
+            ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+        return *this;
+    }
+
+    T &operator[](size_t index) { return ptr[index]; }
+    const T &operator[](size_t index) const { return ptr[index]; }
+
+    T *get() const noexcept { return ptr; }
+
+    T *release() noexcept
+    {
+        T *tmp = ptr;
+        ptr = nullptr;
+        return tmp;
+    }
+
+    void reset(T *p = nullptr) noexcept
+    {
+        if (ptr != p)
+        {
+            release();
             ptr = p;
         }
     }
